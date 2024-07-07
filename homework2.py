4ffb38edffb2a6082cdf791ed0e4fea6d5666776
Karkasha <61556258+yueshh@users.noreply.github.com>
2024-07-07 12:21:18 +0300

    Create homework2.py

---
+++ homework2.py
@@ -0,0 +1,5 @@
+completed_homework = 12
+spent_Hours = 1.5
+name_course = 'Python'
+spent_for_1work = spent_Hours / completed_homework
+print(f'{name_course}, всего задач:{completed_homework}, затрачено часов:{spent_Hours}, среднее время выполнения {spent_for_1work} часа.')
