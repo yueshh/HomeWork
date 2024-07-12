3f27a089569d8ef4d928fbd1e6d2b5ab38e84d59
Karych282ukrf <61556258+yueshh@users.noreply.github.com>
2024-07-12 18:49:25 +0300

    Create module_3_3.py

---
+++ module_3_3.py
@@ -0,0 +1,16 @@
+def print_params(a = 1, b = 'строка', c = True):
+    print(a,b,c)
+
+values_list = [True, 45, 'stroke']
+values_list_2 = [1, 'у']
+values_dict = {'a': 24,
+               'b': True,
+               'c': "Ы"}
+
+print_params()
+print_params(b = 25, c = [1,2,3])
+print_params(a=True)
+print_params(*[0, 2, 1])
+print_params(**values_dict)
+print_params(*values_list)
+print_params(*values_list_2, 42)
